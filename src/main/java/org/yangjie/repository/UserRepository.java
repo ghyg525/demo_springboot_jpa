6e75f72b719aadf993061948144ca40c182ef9df
yangjie <yangjie@beiquan.org>
2017-11-16 16:35:34 +0800

    添加复杂查询处理

--- src/main/java/org/yangjie/repository/UserRepository.java
+++ src/main/java/org/yangjie/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package org.yangjie.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.yangjie.entity.User;
 
@@ -18,10 +19,16 @@ import org.yangjie.entity.User;
  * 自定义sql
  * @Query{@link Query}  @Modifying{@link Modifying}
  * 
+ * JpaRepository 继承于 PagingAndSortingRepository 接口, 拥有PagingAndSortingRepository 的所有方法
+ * 接口特色： 1. 将一些查询方法的返回类型由Iterable 转换成了 List<T>； 2. 新增了保存或更新等方法。
+ * 
+ * JpaSpecificationExecutor 不属于Repository 体系。由于JpaSpecificationExecutor 并不继承repository 接口，所以它不能单独使用，只能和jpa Repository 一起用。
+ * JpaSpecificationExecutor特色：实现了带条件的查询， 类似于Hibernate 的cretira
+ * 
  * @author YangJie 
  * @createTime 2017年11月14日 下午9:47:25
  */
-public interface UserRepository extends JpaRepository<User, Integer>{
+public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User>{
 	
 	/**
 	 * 符合jpa方法名规范, 会自动生成sql
--- src/main/java/org/yangjie/service/UserService.java
+++ src/main/java/org/yangjie/service/UserService.java
@@ -1,8 +1,15 @@
 package org.yangjie.service;
 
 import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.yangjie.entity.User;
 import org.yangjie.repository.UserRepository;
@@ -16,6 +23,35 @@ public class UserService {
 	
 	public List<User> getList(int page, int size) {
 		return userRepository.findAll();
+	}
+	
+	/**
+	 * 模拟复杂条件查询
+	 * @param username
+	 * @param password
+	 * @param teamid
+	 * @return
+	 */
+	public List<User> getList(String username, String password, int teamid){
+		return userRepository.findAll(new Specification<User>() {
+			/**
+			 * @param root: 代表查询的实体类. 
+			 * @param query: 可以从中可到 Root 对象, 即告知 JPA Criteria 查询要查询哪一个实体类. 还可以用来添加查询条件, 还可以结合 EntityManager 对象得到最终查询的 TypedQuery 对象. 
+			 * @param cb: CriteriaBuilder 对象. 用于创建 Criteria 相关对象的工厂. 当然可以从中获取到 Predicate 对象
+			 * @return Predicate 类型, 代表一个查询条件. 
+			 */
+			@Override
+			public Predicate toPredicate(Root<User> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+				if (Objects.nonNull(username)) {
+					cb.and(cb.like(root.get("username"), username));
+				}
+				if (Objects.nonNull(password)) {
+					cb.and(cb.equal(root.get("username"), username));
+				}
+				cb.and(cb.gt(root.get("teamid"), teamid));
+				return cb.conjunction();
+			}
+		});
 	}
 	
 	public long getCount() {
